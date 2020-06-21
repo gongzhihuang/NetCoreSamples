58a66d7db102b05a7246f9f8551d6f8f9856e42c
龚智煌 <gzh@192.168.1.101>
2020-06-21 15:32:14 +0800

    first commit

---
+++ ConfigurationDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConfigurationDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
