42cbf1d6276f8dcf7276da2f2a3d03f518541465
forestGzh <734924401@qq.com>
2020-06-30 12:48:25 +0800

    BackgroundService

---
+++ BackgroundServiceConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace BackgroundServiceConsole
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = new HostBuilder()
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddConsole();
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<TimedBackgroundService>();
+                });
+
+            await builder.RunConsoleAsync();
+        }
+    }
+}
---
+++ BackgroundServiceConsole/TimedBackgroundService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace BackgroundServiceConsole
+{
+    public class TimedBackgroundService : BackgroundService
+    {
+        private readonly ILogger _logger;
+        private Timer _timer;
+
+        public TimedBackgroundService(ILogger<TimedBackgroundService> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            _logger.LogInformation($"Hello World! - {DateTime.Now}");
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            _timer?.Dispose();
+        }
+    }
+}
