f0b9e090dba273152e65e3c41a0560948966994d
forestgzh <734924401@qq.com>
2020-09-07 21:02:20 +0800

    asp.net core引入volo.abp.guids

---
+++ ABPTestGuid/Controllers/WeatherForecastController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Volo.Abp.Guids;
+
+namespace ABPTestGuid.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IGuidGenerator _guidGenerator;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IGuidGenerator guidGenerator)
+        {
+            _logger = logger;
+            _guidGenerator = guidGenerator;
+        }
+
+        //[HttpGet]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    var rng = new Random();
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+
+        [HttpGet]
+        public IEnumerable<Guid> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 100).Select(x => _guidGenerator.Create())
+            .ToArray();
+        }
+    }
+}
